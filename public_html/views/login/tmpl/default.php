a22466b5b059fe78ff39e94ce3ec2ba4e3b11cb8
Everton Alexandre Schuh <everton@easistemas.com.br>
2024-04-02 09:46:47 -0300

    login

--- public_html/views/login/tmpl/default.php
+++ public_html/views/login/tmpl/default.php
@@ -7,17 +7,13 @@ $config   = JFactory::getConfig();
 $siteOffset = $config->getValue('offset');
 
 $filename = 'cache/image_login.png';
-if (!file_exists($filename) || (file_exists($filename) && JFactory::getDate(date ("Y-m-d", filectime($filename)), $siteOffset )->toFormat('%Y-%m-%d', true) > JFactory::getDate('now - 2 days', $siteOffset )->toFormat('%Y-%m-%d', true))) {
+if (!file_exists($filename) || (file_exists($filename) && JFactory::getDate(date ("Y-m-d", filectime($filename)), $siteOffset )->toFormat('%Y-%m-%d', true) < JFactory::getDate('now - 2 days', $siteOffset )->toFormat('%Y-%m-%d', true))) {
 
   jimport('joomla.image.resize');
   $resize = new JResize(); 
   $image = $resize->resize(JPATH_IMAGES.DS.'login'.DS.'atirador00'.rand(1,10).'.jpg', 1250, 1200, 'cache/image_login.png');
 
 }			
-
-
-echo JFactory::getDate(date ("Y-m-d", filectime($filename)), $siteOffset )->toFormat('%Y-%m-%d', true);
-exit;
 
 ?>
 <style>
